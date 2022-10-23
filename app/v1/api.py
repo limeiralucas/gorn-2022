b1073d105fa9caf5550ae4e87f48f5b8989d50b3
Lucas Limeira <lucasalveslm@gmail.com>
2022-10-23 16:37:36 -0300

    added version endpoint

--- app/v1/api.py
+++ app/v1/api.py
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.get('/')
 async def root() -> Dict:
     return dict(status='ok')
+
+
+@router.get('/version')
+async def version() -> Dict:
+    return dict(version='1')
